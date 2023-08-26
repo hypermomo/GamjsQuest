8eed13039d7c3d9519a3e3a460727b3b53ceddc5
bykum1 <bykum1@hanmail.net>
2023-08-26 18:43:13 +0900

    감자 퀘스트 메이킹 시작

---
+++ src/main/java/gamja/quest/plugin/Main.java
@@ -0,0 +1,12 @@
+package gamja.quest.plugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+    }
+    @Override
+    public void onDisable() {
+    }
+}
